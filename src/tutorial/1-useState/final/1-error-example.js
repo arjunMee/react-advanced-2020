35f20141e29621d7a6db98defc0f36176b8733b7
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-05-21 13:09:59 +0530

    Update 1-error-example.js

--- src/tutorial/1-useState/final/1-error-example.js
+++ src/tutorial/1-useState/final/1-error-example.js
@@ -1,20 +1,20 @@
-import React from 'react';
+import React from 'react'
 
 const ErrorExample = () => {
-  let title = 'random title';
+  let title = 'random title'
 
   const handleClick = () => {
-    title = 'hello people';
-    console.log(title);
-  };
+    title = 'hello people'
+    console.log(title)
+  }
   return (
     <React.Fragment>
       <h2>{title}</h2>
       <button type='button' className='btn' onClick={handleClick}>
-        change title
+        change p
       </button>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default ErrorExample;
+export default ErrorExample
