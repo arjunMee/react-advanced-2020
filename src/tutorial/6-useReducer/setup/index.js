b061362e0c52f56f5a2a25d5384e744647d75e79
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-05-21 20:35:24 +0530

    Update index.js

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -1,10 +1,51 @@
-import React, { useState, useReducer } from 'react';
-import Modal from './Modal';
-import { data } from '../../../data';
+import React, { useState, useReducer } from 'react'
+import Modal from './Modal'
+import { data } from '../../../data'
 // reducer function
 
+let reducer = (state, action) => {}
+let defaultState = {
+  //inintial state
+  people: [],
+  isModelOpen: false,
+  modelaContent: 'hello word',
+}
 const Index = () => {
-  return <h2>useReducer</h2>;
-};
+  const [name, setName] = useState('')
+  const [state, dispatch] = useReducer(reducer, defaultState)
 
-export default Index;
+  let handleSubmit = (e) => {
+    e.preventDefault()
+    if (name) {
+      setshowModel(true)
+      setPeople([...people, { id: new Date().getTime().toString(), name }])
+    } else {
+      setshowModel(true)
+    }
+  }
+
+  return (
+    <>
+      {showModel && <Modal />}
+      <form className='form' onSubmit={handleSubmit}>
+        <div>
+          <input
+            type='text'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          ></input>
+        </div>
+        <button type='submit'>add</button>
+      </form>
+      {people.map(({ id, name }) => {
+        return (
+          <div>
+            <h3>{name}</h3>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default Index
