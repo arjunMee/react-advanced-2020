d4cf9aaeccda8a082a92b392b8e528135bc2b503
arjunMee <70093722+arjunMee@users.noreply.github.com>
2021-05-22 00:47:40 +0530

    s

--- src/tutorial/6-useReducer/setup/index.js
+++ src/tutorial/6-useReducer/setup/index.js
@@ -2,13 +2,28 @@ import React, { useState, useReducer } from 'react'
 import Modal from './Modal'
 import { data } from '../../../data'
 // reducer function
+let reducer = (state, action) => {
+  const newAr = [...state.people, action.payLoad]
+  if (action.type === 'ADD_ITEM') {
+    return {
+      ...state,
+      people: newAr,
+      isModelOpen: true,
+      modelaContent: 'item added',
+    }
+  }
 
-let reducer = (state, action) => {}
+  if (action.type === 'NO_VALUE') {
+    return { ...state, isModelOpen: true, modelaContent: 'please ad dsgdg' }
+  }
+  // return state
+  throw new Error('np match found')
+}
+//inintial state
 let defaultState = {
-  //inintial state
   people: [],
   isModelOpen: false,
-  modelaContent: 'hello word',
+  modelaContent: '',
 }
 const Index = () => {
   const [name, setName] = useState('')
@@ -17,16 +32,21 @@ const Index = () => {
   let handleSubmit = (e) => {
     e.preventDefault()
     if (name) {
-      setshowModel(true)
-      setPeople([...people, { id: new Date().getTime().toString(), name }])
+      // setshowModel(true)
+      // setPeople([...people, { id: new Date().getTime().toString(), name }])
+      dispatch({
+        type: 'ADD_ITEM',
+        payLoad: { id: new Date().getTime().toString(), name },
+      })
     } else {
-      setshowModel(true)
+      dispatch({ type: 'NO_VALUE' })
+      // setshowModel(true)
     }
   }
 
   return (
     <>
-      {showModel && <Modal />}
+      {state.isModelOpen && <Modal modelaContent={state.modelaContent} />}
       <form className='form' onSubmit={handleSubmit}>
         <div>
           <input
@@ -37,10 +57,10 @@ const Index = () => {
         </div>
         <button type='submit'>add</button>
       </form>
-      {people.map(({ id, name }) => {
+      {state.people.map(({ id, name }) => {
         return (
-          <div>
-            <h3>{name}</h3>
+          <div key={id}>
+            <h3>'hello' + {name} + 'plus'</h3>
           </div>
         )
       })}
@@ -49,3 +69,49 @@ const Index = () => {
 }
 
 export default Index
+
+// import React, { useState, useReducer } from 'react'
+// import Modal from './Modal'
+// import { data } from '../../../data'
+// // reducer function
+
+// const Index = () => {
+//   const [name, setName] = useState('')
+//   const [people, setPeople] = useState(data)
+//   const [showModel, setshowModel] = useState(false)
+
+//   let handleSubmit = (e) => {
+//     e.preventDefault()
+//     if (name) {
+//       setshowModel(true)
+//       setPeople([...people, { id: new Date().getTime().toString(), name }])
+//     } else {
+//       setshowModel(true)
+//     }
+//   }
+
+//   return (
+//     <>
+//       {showModel && <Modal />}
+//       <form className='form' onSubmit={handleSubmit}>
+//         <div>
+//           <input
+//             type='text'
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//           ></input>
+//         </div>
+//         <button type='submit'>add</button>
+//       </form>
+//       {people.map(({ id, name }) => {
+//         return (
+//           <div>
+//             <h3>{name}</h3>
+//           </div>
+//         )
+//       })}
+//     </>
+//   )
+// }
+
+// export default Index
